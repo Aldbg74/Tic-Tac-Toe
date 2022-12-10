765f2dd01d128ace3527d62fb4cdae3f1562735c
Alexis <63512507+Aldbg74@users.noreply.github.com>
2022-12-10 16:43:19 +0100

    Update # Tic Tac Toe #.py

--- # Tic Tac Toe #.py
+++ # Tic Tac Toe #.py
@@ -17,7 +17,8 @@ print ("")
 
 
 class TicTacToe:
-    self.board = []
+    def __init__(self):
+        self.board = []
 
 def creqte_board():
     for i in range(3):
@@ -90,3 +91,52 @@ def is_board_filled(self):
             if item == "-":
                 return False
     return True
+
+def swap_player_turn(self, player):
+    return 'x' if player == 'o' else 'o'
+
+def show_board(self):
+    for row in self.board:
+        for item in row:
+            print(item, end=" ")
+        print()
+
+def start(self):
+    self.create_board()
+
+    player = 'x' if self.get_random_first_player() == 1 else 'o'
+    while True:
+        print(f"player {player} turn")
+
+        self.show_board()
+
+        # Taking the user input
+        row, col = list(
+                map(int, input("Enter row and column numbers to fix spot: ").split()))
+        print()
+
+        # fixing the spot
+        self.fix_spot(row - 1, col - 1, player)
+
+            # checking whether current player is won or not
+        if self.is_player_win(player):
+                print(f"Player {player} wins the game!")
+                break
+
+            # checking whether the game is draw or not
+        if self.is_board_filled():
+                print("Match Draw!")
+                break
+
+            # swapping the turn
+        player = self.swap_player_turn(player)
+
+        # showing the final view of board
+        print()
+        self.show_board()
+
+
+# starting the game 
+# DU COUP SA MERDE ICI
+tic_tac_toe = TicTacToe()
+tic_tac_toe.start()
