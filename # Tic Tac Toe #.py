297f9cf302bf4f8086b930f6c2801aca06cacf3b
Alexis <63512507+Aldbg74@users.noreply.github.com>
2022-12-10 15:40:58 +0100

    Create # Tic Tac Toe #.py

---
+++ # Tic Tac Toe #.py
@@ -0,0 +1,34 @@
+# Tic Tac Toe #
+# By Alexis.D AKA Aldbg74 #
+# 2022 #
+
+import random
+print ("Welcome to Tic Tac Toe")
+print ("You need to use coordonates to play")
+print ("The coordonates are like that")
+print ("1 2 3")
+print ("1 - - ")
+print ("2 - - ")
+print ("3 - - ")
+print ("So for putting a synbol in the middle of the board you need to type 2 2")
+
+class TicTacToe:
+    self.board = []
+
+def creqte_board():
+    for i in range(3):
+        row = []
+        for j in range(3):
+            row.append("-")
+        self.board.append(row)
+
+def get_random_first_player(self):
+    return randon.randint(0, 1)
+
+def fix_spot(self, row, col, player):
+    self.board[row][col] = player
+
+def is_player_win(self, player):
+    win = None
+
+    n = len(self.board)
